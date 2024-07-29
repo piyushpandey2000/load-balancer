79dd46bd63b153ddae369cac068d8d856ac249e1
piyushpandey2000 <piyushpandeywayne@gmail.com>
2024-07-30 03:40:56 +0530

    initial code for BackendServer

--- src/main/java/resource/BackendServer.java
+++ src/main/java/resource/BackendServer.java
@@ -1,4 +1,19 @@
 package resource;
 
 public class BackendServer {
+    private final String host;
+    private final int port;
+
+    public BackendServer(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
